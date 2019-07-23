cff60275e6aa2d77431d1441fcee735c37c37e1b
Dion Roloff <dion.roloff@gmail.com>
2019-07-23 14:01:56 -0500

    fetch coin data

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet } from "react-native";
 const Header = () => {
   return (
     <View style={ styles.headerContainer }>
-      <Text style={ styles.header }>Tracker</Text>
+      <Text style={ styles.header }>CryptoUpdates</Text>
     </View>
   );
 };
